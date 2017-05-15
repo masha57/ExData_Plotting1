d3c30bb7c758ee509d12e3f71be6033ef459582b
masha57 <mas.avdeeva@gmail.com>
2017-05-15 18:23:24 -0400

    Masha's Project 1

--- plot1.R
+++ plot1.R
@@ -1,3 +1,6 @@
+URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(URL, "household_power_consumption.zip")
+unzip("household_power_consumption.zip", "household_power_consumption.txt")
 mydata <- fread("household_power_consumption.txt", sep = ";", header = TRUE, 
                 na.strings = "?")
 mydata_n <- subset(mydata, Date == "1/2/2007" | Date == "2/2/2007")
